5ddd9c6a893564caae34475ed651e5c59a8e0acc
ArnSch <arnaud.schenk@gmail.com>
2014-08-06 14:14:18 +0200

    remove all frontend

--- app/views/emails/contact.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        Message: {{$data}}
-    </body>
-</html>
--- app/views/shop/include/productCard.blade.php
+++
@@ -1,5 +0,0 @@
-	<li>
-		<a href="/products/{{ $product->sku }}">
-	    <img src=" {{{ $product->img_link }}}" alt=" {{ $product->name }} ">
-	    <p>View Details</p>
-	</a> <!-- Yes, there is a missing closing tag for whitespace reasons, because html is stupid. -->
